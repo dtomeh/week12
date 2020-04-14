0a19ce43063e79c5c94b0be0b14bb02ff815f1bf
dtomeh <tomeh001@umn.edu>
2020-04-14 10:39:01 -0500

    Commented about optimization

--- R/local.R
+++ R/local.R
@@ -3,8 +3,8 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 # Libraries
 library(tidyverse)
+library(caret)
 library(haven)
-library(foreign)
 library(xgboost)
 library(parallel)
 library(doParallel)
@@ -12,22 +12,47 @@ library(microbenchmark)
 library(tictoc)
 
 # Data Import and Cleaning 
-data <- read.spss("../data/GSS2006.sav")
+  week12 <- read_sav("../data/GSS2006.sav")
+  week12_tbl <-as_tibble(week12) %>% 
+    select(HEALTH, BIG5A1, BIG5B1, BIG5C1, BIG5D1, BIG5E1, BIG5A2, BIG5B2, BIG5C2, BIG5D2, BIG5E2) %>%
+    filter(rowSums(is.na(.[,1:11]))!=11)  %>%
+    filter(rowSums(is.na(.[,2:11]))!=10) %>%
+    filter(is.na(HEALTH)==FALSE) %>%
+    mutate_all(as.numeric)
 
-data_tbl <-data %>%
-  as_tibble %>%
-  select(starts_with("BIG5"), HEALTH) %>%
-  mutate_all(~ na_if(., "IAP")) %>%
-  mutate_all(~ na_if(., "CANT CHOOSE")) %>%
-  mutate_all(~ na_if(., "NA")) %>%
-  mutate_all(~ na_if (., "DK")) %>%
-  mutate_all(as.numeric)
+#Analysis 
+exec_time_np <- system.time({
+xgb_mod <- train(
+  HEALTH ~ .^3, 
+  week12_tbl, 
+  method="xgbTree",
+  preProcess=c("center", "scale", "zv", "medianImpute"), 
+  trControl= trainControl(method="cv", number = 10, verboseIter = T), 
+  tuneLength=2,
+  na.action = na.pass
+ 
+)
+})
 
-missing_big5_tbl <- data_tbl %>%
-  select(starts_with("BIG5")) %>%
-  mutate_all(~ ifelse(is.na(.), 1, 0))
-data_tbl$missings <- rowSums(missing_big5_tbl)
+detectCores()
+local_cluster <-makeCluster(3)
+registerDoParallel(local_cluster)
 
-final_tbl <- data_tbl %>%
-  filter(missings <7) %>%
-  select(-missings)
+exec_time_p <- system.time({xgb_mod2 <- train(
+  HEALTH ~ .^3, 
+  week12_tbl, 
+  method="xgbTree",
+  preProcess=c("center", "scale", "zv", "medianImpute"), 
+  trControl= trainControl(method="cv", number = 10, verboseIter = T), 
+  tuneLength=2,
+  na.action = na.pass
+  
+)
+})
+stopCluster(local_cluster) 
+registerDoSEQ()
+
+## Finding the difference in time between the parallelized and non-parallelized xgb models
+exec_time_np-exec_time_p
+# In the non-parallelized version of the model the user time is 112.120, the system time is 2.857, and the total time elapsed is 116.852. 
+# In the parallelized version of the model the user time is 1.458 seconds, the system time is .0327, and the total time elapsed is 70.202
---
+++ msi/interactive.R
@@ -0,0 +1,75 @@
+# Libraries
+library(caret) #for train
+library(parallel) #to parallelize
+library(doParallel) #to parallelize
+library(xgboost) #xgb model
+library(readr) #write csv
+library(tibble) #save as tibble
+library(haven) #import spss
+
+# Data Import and Cleaning 
+
+#using read.sav eliminates the need to remove strange values and reformat missingness and strange values as NAs, it does it automatically. It also refactors the values of the 5 point scale to numeric. Its ultimately about .3 of second faster, but does involve a lot less code. 
+data_int <- read_sav("GSS2006.sav")
+#removing the need for tidyverse eliminated 3 seconds on average from the data import and cleaning portion of the code
+data_int_tbl <- data_int[,c("HEALTH", "BIG5A1", "BIG5A2", "BIG5B1", "BIG5B2", "BIG5C1", "BIG5C2", "BIG5D1", "BIG5D2", "BIG5E1", "BIG5E2")]
+
+data_int_tbl$HEALTH <- as.numeric(data_int_tbl$HEALTH)
+data_int_tbl$BIG5A1 <- as.numeric(data_int_tbl$BIG5A1)
+data_int_tbl$BIG5A2 <- as.numeric(data_int_tbl$BIG5A2)
+data_int_tbl$BIG5B1 <- as.numeric(data_int_tbl$BIG5B1)
+data_int_tbl$BIG5B2 <- as.numeric(data_int_tbl$BIG5B2)
+data_int_tbl$BIG5C1 <- as.numeric(data_int_tbl$BIG5C1)
+data_int_tbl$BIG5C2 <- as.numeric(data_int_tbl$BIG5C2)
+data_int_tbl$BIG5D1 <- as.numeric(data_int_tbl$BIG5D1)
+data_int_tbl$BIG5D2 <- as.numeric(data_int_tbl$BIG5D2)
+data_int_tbl$BIG5E1 <- as.numeric(data_int_tbl$BIG5E1)
+data_int_tbl$BIG5E2 <- as.numeric(data_int_tbl$BIG5E2)
+
+#converting each variable to numeric separately as opposed to as_tibble(lapply(data_int_tbl, as.numeric)) eliminated 1.5 seconds from the overall time of data import and cleaning
+
+
+int_missing <- data_int_tbl[!is.na(data_int_tbl$HEALTH),]
+int_missing$big5Sums <- rowSums(is.na(int_missing))
+int_no_missing <- int_missing[!(int_missing$big5Sums==10),]
+
+
+#using system.time instead of microbenchmark saves a lot of time since iterations of the code are eliminated.Since the goal is to minimize the run time of the overall code while saving the run time of this chunk, i decied to use system.time. 
+
+#Analysis 
+exec_time_nonparallelized <- system.time({
+  xgb_mod <- train(
+    HEALTH ~ .^3, 
+    int_no_missing, 
+    method="xgbTree",
+    preProcess=c("center", "scale", "zv", "medianImpute"), 
+    trControl= trainControl(method="cv", number = 10, verboseIter = T), 
+    tuneLength=3,
+    na.action = na.pass
+    
+  )
+})
+
+detectCores()
+local_cluster <-makeCluster(2)
+registerDoParallel(local_cluster)
+
+exec_time_parallelized <- system.time({xgb_mod2 <- train(
+  HEALTH ~ .^3, 
+  int_no_missing, 
+  method="xgbTree",
+  preProcess=c("center", "scale", "zv", "medianImpute"), 
+  trControl= trainControl(method="cv", number = 10, verboseIter = T), 
+  tuneLength=3,
+  na.action = na.pass
+  
+)
+})
+stopCluster(local_cluster) 
+registerDoSEQ()
+
+## Finding the difference in time between the parallelized and non-parallelized xgb models
+exec_time_nonparallelized-exec_time_parallelized
+# In the non-parallelized version of the model the user time is 306.541, the system time is 1.108, and the total time elapsed is 309.558 
+# In the parallelized version of the model the user time is 1.566 seconds, the system time is .070, and the total time elapsed is 339.654
+write_csv(tibble(nonparallelized = exec_time_nonparallelized, paralellized=exec_time_parallelized), "../interactive.csv", col_names=TRUE, append=TRUE)
